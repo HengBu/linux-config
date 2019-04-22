1643465331f82b78b15300937bfbc3ed369a7763
卜衡 <1533924095@qq.com>
2019-04-22 09:01:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 #copu tmux config
 mv ./tmux/tmux ./tmux/.tmux
-cp -r ./tmux/* ~
+cp -r ./tmux/.* ~
 
 #copy vim config
 mv ./vim/vimrc ./vim/.vimrc
 mv ./vim/vim ./vim/.vim
-cp -r ./vim/* ~
+cp -r ./vim/.* ~
 
 
